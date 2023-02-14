a52bbe38db309bef12121a6372fe45a369d6a87e
Varun Verma <varunverma855@gmail.com>
2023-02-13 20:43:58 -0500

    Some closure

---
+++ rust-book/chapter-13/target/debug/chapter-13.d
@@ -0,0 +1,1 @@
+/Users/varunverma/Desktop/rust/rust-playground/rust-book/chapter-13/target/debug/chapter-13: /Users/varunverma/Desktop/rust/rust-playground/rust-book/chapter-13/src/main.rs
---
+++ rust-book/chapter-13/target/debug/deps/chapter_13-a58ff2bb10cf8cce.d
@@ -0,0 +1,5 @@
+/Users/varunverma/Desktop/rust/rust-playground/rust-book/chapter-13/target/debug/deps/chapter_13-a58ff2bb10cf8cce: src/main.rs
+
+/Users/varunverma/Desktop/rust/rust-playground/rust-book/chapter-13/target/debug/deps/chapter_13-a58ff2bb10cf8cce.d: src/main.rs
+
+src/main.rs:
